ab558c4579fedb9477c8ca2a564416d7d1f73d29
Baris Ciftci <barishciftci@gmail.com>
2022-05-17 21:48:08 +0200

    Assets material and background gradient have been added.

--- DiceApp/ViewController.swift
+++ DiceApp/ViewController.swift
@@ -9,14 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        //Background gradient color
+        let backgroundGradientColor = CAGradientLayer()
+        
+        backgroundGradientColor.colors = [
+            UIColor(red: 41.0 / 255.0,
+                    green: 101.0 / 255.0,
+                    blue: 34.0 / 255.0,
+                    alpha: 1.0).cgColor,
+            UIColor(red: 11.0 / 255.0,
+                    green: 27.0 / 255.0,
+                    blue: 9.0 / 255.0,
+                    alpha: 1.0).cgColor]
+        
+        backgroundGradientColor.frame = view.frame
+        backgroundGradientColor.startPoint = CGPoint(x: 0.0, y: 0.0)
+        backgroundGradientColor.endPoint = CGPoint(x: 1.0, y: 1.0)
+        
+        view.layer.addSublayer(backgroundGradientColor)
+        
     }
     
-   
     
-
-
+    
+    
+    
+    
 }
+
+
 
