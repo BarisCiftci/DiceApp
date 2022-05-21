183e64f7c2953bbdcb7d6fc7560e141cdd426ff1
Baris Ciftci <barishciftci@gmail.com>
2022-05-21 13:19:09 +0200

    Dice images have been added, connected to the button to be viewed randomly.

--- DiceApp/ViewController.swift
+++ DiceApp/ViewController.swift
@@ -8,38 +8,56 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    
+    //Ui element reference (image views)
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //diceImageViewOne.image = UIImage(named: "DiceOne")
+        //diceImageViewTwo.image = UIImage(named: "DiceSix")
         
         //Background gradient color
+        
+        let lightGreen = UIColor(red: 41.0 / 255.0 ,
+                                 green: 101.0 / 255.0,
+                                 blue: 34.0 / 255.0,
+                                 alpha: 1.0).cgColor
+        
+        let darkGreen = UIColor(red: 11.0 / 255.0 ,
+                                green: 27.0 / 255.0,
+                                blue: 9.0 / 255.0,
+                                alpha: 1.0).cgColor
+        
         let backgroundGradientColor = CAGradientLayer()
         
-        backgroundGradientColor.colors = [
-            UIColor(red: 41.0 / 255.0,
-                    green: 101.0 / 255.0,
-                    blue: 34.0 / 255.0,
-                    alpha: 1.0).cgColor,
-            UIColor(red: 11.0 / 255.0,
-                    green: 27.0 / 255.0,
-                    blue: 9.0 / 255.0,
-                    alpha: 1.0).cgColor]
+        backgroundGradientColor.colors = [lightGreen, darkGreen]
         
         backgroundGradientColor.frame = view.frame
         backgroundGradientColor.startPoint = CGPoint(x: 0.0, y: 0.0)
         backgroundGradientColor.endPoint = CGPoint(x: 1.0, y: 1.0)
         
-        view.layer.addSublayer(backgroundGradientColor)
-        
+        self.view.layer.insertSublayer(backgroundGradientColor, at: 0)
     }
     
-    
-    
-    
-    
+    //Button Tapped action
+    @IBAction func rollButtonPressed(_ sender: Any) {
+        
+        let diceArray = [
+            UIImage(named: "DiceOne"),
+            UIImage(named: "DiceTwo"),
+            UIImage(named: "DiceThree"),
+            UIImage(named: "DiceFour"),
+            UIImage(named: "DiceFive"),
+            UIImage(named: "DiceSix")
+        ]
+        
+        diceImageViewOne.image = diceArray[Int.random(in: 0...5)]
+        
+        diceImageViewTwo.image = diceArray[Int.random(in: 0...5)]
+        
+    }
     
 }
 
