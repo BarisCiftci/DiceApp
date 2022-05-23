75effd1422b7839c39cad806b0a81f5fb58ae76e
Baris Ciftci <barishciftci@gmail.com>
2022-05-23 22:49:50 +0200

    RollButtonPressed, updated

--- DiceApp/ViewController.swift
+++ DiceApp/ViewController.swift
@@ -15,20 +15,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //diceImageViewOne.image = UIImage(named: "DiceOne")
-        //diceImageViewTwo.image = UIImage(named: "DiceSix")
-        
         //Background gradient color
         
-        let lightGreen = UIColor(red: 41.0 / 255.0 ,
-                                 green: 101.0 / 255.0,
-                                 blue: 34.0 / 255.0,
-                                 alpha: 1.0).cgColor
+        let lightGreen = UIColor(
+            red: 41.0 / 255.0,
+            green: 101.0 / 255.0,
+            blue: 34.0 / 255.0,
+            alpha: 1.0
+        ).cgColor
         
-        let darkGreen = UIColor(red: 11.0 / 255.0 ,
-                                green: 27.0 / 255.0,
-                                blue: 9.0 / 255.0,
-                                alpha: 1.0).cgColor
+        let darkGreen = UIColor(
+            red: 11.0 / 255.0,
+            green: 27.0 / 255.0,
+            blue: 9.0 / 255.0,
+            alpha: 1.0
+        ).cgColor
         
         let backgroundGradientColor = CAGradientLayer()
         
@@ -39,9 +40,12 @@ class ViewController: UIViewController {
         backgroundGradientColor.endPoint = CGPoint(x: 1.0, y: 1.0)
         
         self.view.layer.insertSublayer(backgroundGradientColor, at: 0)
+        
+        
     }
     
     //Button Tapped action
+    
     @IBAction func rollButtonPressed(_ sender: Any) {
         
         let diceArray = [
@@ -57,10 +61,12 @@ class ViewController: UIViewController {
         diceImageViewTwo.image = diceArray[Int.random(in: 0...5)]
         
         // Random rotation
-        diceImageViewOne.transform = CGAffineTransform(rotationAngle: CGFloat(Int.random(in: 0...Int(Double(20626.5)))))
-        diceImageViewTwo.transform = CGAffineTransform(rotationAngle: CGFloat(Int.random(in: 0...Int(Double(20626.5)))))
+        diceImageViewOne.transform = CGAffineTransform(rotationAngle: CGFloat(Int.random(in: 0...360)))
+        diceImageViewTwo.transform = CGAffineTransform(rotationAngle: CGFloat(Int.random(in: 0...360)))
+        
         
     }
+   
     
 }
 
