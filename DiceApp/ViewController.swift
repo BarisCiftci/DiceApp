f94f361c11a81d0c100bed433f1959ffe0385696
Baris Ciftci <barishciftci@gmail.com>
2022-08-23 22:26:46 +0200

    ImageView position feature and border stroke line have been added.

--- DiceApp/ViewController.swift
+++ DiceApp/ViewController.swift
@@ -8,9 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    
     //Ui element reference (image views)
     @IBOutlet weak var diceImageViewOne: UIImageView!
     @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        diceImageViewOne.layer.borderColor = UIColor.black.cgColor
+        diceImageViewOne.layer.borderWidth = 2
+        diceImageViewOne.layer.cornerRadius = 18
+        
+        diceImageViewTwo.layer.borderColor = UIColor.black.cgColor
+        diceImageViewTwo.layer.borderWidth = 2
+        diceImageViewTwo.layer.cornerRadius = 18
+
+     }
     
     let diceArray = [
         UIImage(named: "DiceOne"),
@@ -22,39 +39,48 @@ class ViewController: UIViewController {
     ]
     override var canBecomeFirstResponder: Bool {
         return true
+        
     }
+    
     
     override func motionBegan(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
         if motion == .motionShake {
+            RandomDice()
             
-            DiceMotionStart()
         }
     }
     
     override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
         
         if motion == .motionShake {
-            
-            DiceMotionEnd()
+            RandomDiceRotation()
+            RandomDicePosition()
         }
-        
     }
     
-    
-    func DiceMotionStart() {
-        
+    func RandomDice() {
         // Random Image
         diceImageViewOne.image = diceArray[Int.random(in: 0...5)]
         diceImageViewTwo.image = diceArray[Int.random(in: 0...5)]
         
+        
     }
-    
-    func DiceMotionEnd() {
+    func RandomDiceRotation() {
         // Random rotation
         diceImageViewOne.transform = CGAffineTransform(rotationAngle: CGFloat(Int.random(in: 0...360)))
         diceImageViewTwo.transform = CGAffineTransform(rotationAngle: CGFloat(Int.random(in: 0...360)))
         
+        
     }
+    
+    func RandomDicePosition() {
+        //Random position
+        diceImageViewOne.layer.position = CGPoint(x: Int.random(in: 100...200), y: Int.random(in: 200...600))
+        diceImageViewTwo.layer.position = CGPoint(x: Int.random(in: 100...200), y: Int.random(in: 200...600))
+    }
+    
+    
+    
     
     
     
